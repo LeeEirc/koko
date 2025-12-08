0752a8c9600e28e085b9e30936039077e7fed4b3
Eric <xplzv@126.com>
2025-12-08 14:58:11 +0800

    perf: remove sleep

--- ui/src/hooks/useTerminalSocket.ts
+++ ui/src/hooks/useTerminalSocket.ts
@@ -428,9 +428,9 @@ export const useTerminalSocket = () => {
       terminalRef.value.write(`\r\n`);
       terminalRef.value.write(`\x1B[31m${t('WebSocketClosed')}\x1B[0m`);
     };
-    const sleep = (ms: number) => new Promise<void>(r => setTimeout(r, ms));
+    // const sleep = (ms: number) => new Promise<void>(r => setTimeout(r, ms));
     socketRef.value.onmessage = async (message: MessageEvent) => {
-      await sleep(1); // time sleep 0.001, avoid long write and block websocket send
+      // await sleep(1); // time sleep 0.001, avoid long write and block websocket send
       lastReceiveTime.value = new Date();
       if (typeof message.data === 'object') {
         handleBinaryMessage(message);
